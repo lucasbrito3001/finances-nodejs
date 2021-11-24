88cd6405f3b17943b27c63a7bcc2466aeb6b6ede
Lucas de Brito <lucas.debrito@lexartlabs.com>
2021-11-24 00:58:28 -0300

    env

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     process.env.DB_DATABASE,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    { dialect: process.env.DB_DIALECT, host: process.env.DB_HOST }
+    { dialect: 'postgres', host: process.env.DB_HOST }
 );
 
 // sequelize.authenticate().then(() => {
