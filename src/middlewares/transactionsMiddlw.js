748fc29b1895d7157741f0f7dda21deda59e02f8
dev-lucas <devlucasmail@gmail.com>
2021-12-15 09:31:56 -0300

    Fix middleware # fix: Verify only for the transactions completed

--- src/middlewares/transactionsMiddlw.js
+++ src/middlewares/transactionsMiddlw.js
@@ -3,8 +3,10 @@ import * as res from "node_modules/express/lib/response"
 const middlewares = {
     checkIfHaveSufficientMoney: function (userTransactions, newTransaction, res) {
         const userBalance = userTransactions.reduce((prev, next) => {
-            if(next.transactionId === 2) prev + next.amount
-            else if(next.transactionId === 3) prev - next.amount
+            if(next.transactionStatus === 'completed') {
+                if(next.transactionId === 2) prev + next.amount
+                else if(next.transactionId === 3) prev - next.amount
+            }
         }, 0)
         if(userBalance >= newTransaction.amount) return
         else return res.status(406).json({
